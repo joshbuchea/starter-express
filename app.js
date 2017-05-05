41c7943bbb922401de06426a5737501c7adfa75a
Josh Buchea <verctrl@joshbuchea.com>
2017-05-05 14:17:48 -0700

    Use localhost instead of 0.0.0.0

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 // *** START THE SERVER *** //
-const server = app.listen(3000, '0.0.0.0', function () {
+const server = app.listen(3000, 'localhost', function () {
   const host = server.address().address;
   const port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
